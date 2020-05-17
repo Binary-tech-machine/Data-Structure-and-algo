2faaf24be32e7c4bf8e10847de3e781c1ee4dcc4
Harsh Agarwal <54469226+Binary-tech-machine@users.noreply.github.com>
2020-05-18 02:10:26 +0530

    code of number of combination

---
+++ ncr.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fact(int n)
+{
+	if(n==0)
+ return 1;
+ return fact(n-1)*n;
+}
+int ncr(int n, int r)
+{
+ void num ,deno;
+ num=fact(n);
+ deno=fact(r)*fact(n-r);
+ return num/deno;
+}
+int main()
+{
+  int n,r;
+  printf("enter the value of n and r");
+  scanf("%d%d",&n,&r);
+  printf("%d",ncr(n,r));
+  return 0;
+} 
