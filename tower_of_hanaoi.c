53d9795c27cd6bc4a3db303f69afafee940c8272
Harsh Agarwal <54469226+Binary-tech-machine@users.noreply.github.com>
2020-05-19 17:02:23 +0530

    tower of hanaoi solved

--- tower_of_hanaoi.c
+++ tower_of_hanaoi.c
@@ -2,12 +2,15 @@
 void TOH (int n, char a, char b, char c){
   if(n>0){
     TOH (n-1,a,c,b);
-    printf("\nfrom %c to %c",a,c);
+    printf("\nfrom disc %c to disc %c",a,c);
     TOH(n-1,b,a,c);
   }
 } 
 int main (){
-  TOH(4,65,66,67);
-  printf("\n finally all the disc are transfer from tower A to tower C");
+  int n;
+  printf("enter the number of disc:");
+  scanf("%d",&n);
+  TOH(n,65,66,67);
+  printf("\nfinally all the disc are transfer from tower A to tower C");
   return 0;
 }
