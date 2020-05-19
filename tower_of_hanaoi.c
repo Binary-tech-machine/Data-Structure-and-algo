809040855f4662912143af3125ea7f209a94a86e
Harsh Agarwal <54469226+Binary-tech-machine@users.noreply.github.com>
2020-05-19 17:03:58 +0530

    solved

--- tower_of_hanaoi.c
+++ tower_of_hanaoi.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-void TOH (int n, char a, char b, char c){
-  if(n>0){
+void TOH (int n, char a, char b, char c)
+{
+  if(n>0)
+  {
     TOH (n-1,a,c,b);
     printf("\nfrom disc %c to disc %c",a,c);
     TOH(n-1,b,a,c);
   }
 } 
-int main (){
+int main ()
+{
   int n;
   printf("enter the number of disc:");
   scanf("%d",&n);
