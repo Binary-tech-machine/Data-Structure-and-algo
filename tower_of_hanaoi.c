94dda925c704a916466a87325597b1bf0469df70
Harsh Agarwal <54469226+Binary-tech-machine@users.noreply.github.com>
2020-05-19 16:51:52 +0530

    tower of hanaoi

--- tower_of_hanaoi.c
+++ tower_of_hanaoi.c
@@ -2,13 +2,11 @@
 void TOH (int n, int a, int b, int c){
   if(n>0){
     TOH (n-1,a,c,b);
-    printf("\nfrom %d to %d using c",a,c);
+    printf("\nfrom %d to %d",a,c);
     TOH(n-1,b,a,c);
-    printf("\nfrom %d to %d using a",b,a);
   }
 } 
-int main ()
-  {
+int main (){
   TOH(2,1,2,3);
   return 0;
 }
